713e0d995199992f83eb1f313e9fa9a5b7bff82b
growfinixpvtltd <growfinixpvtltd@growfinix.in>
2026-01-11 23:13:28 +0530

    dist folder changes

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,7 @@ const EditProductRoute = require("./routes/editProduct.routes.js");
 const ShowcaseRoute = require("./routes/showcaseRoutes.js");
 
 const app = express();
+console.log("API BASE URL 👉", import.meta.env.VITE_API_BASE_URL);
 
 // --------------------
 // Middlewares
